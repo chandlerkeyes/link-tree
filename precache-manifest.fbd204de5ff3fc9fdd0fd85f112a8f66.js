412a575cd22bd18ff06edbf1e834ee34566e825b
Chandler <chandler@kodeably.com>
2021-10-14 12:29:18 -0500

    Updates

---
+++ precache-manifest.fbd204de5ff3fc9fdd0fd85f112a8f66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad8e50eb14c707d0552bf8ddcf76e7f",
+    "url": "/link-tree/index.html"
+  },
+  {
+    "revision": "2da7b4ddb5d9fe53706b",
+    "url": "/link-tree/static/css/main.c7f9bcee.chunk.css"
+  },
+  {
+    "revision": "f377f7b9304d8b88b6c4",
+    "url": "/link-tree/static/js/2.ddb7c8e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/link-tree/static/js/2.ddb7c8e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da7b4ddb5d9fe53706b",
+    "url": "/link-tree/static/js/main.8f884c37.chunk.js"
+  },
+  {
+    "revision": "7169a9fe1c5bc2672a0d",
+    "url": "/link-tree/static/js/runtime-main.e5dab583.js"
+  },
+  {
+    "revision": "19672a43857673da715f13229d54e8d1",
+    "url": "/link-tree/static/media/selfie.19672a43.jpg"
+  }
+]);
